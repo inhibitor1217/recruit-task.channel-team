3ea75e8d51e243b33a872d1cd01ca9835cb4ec20
inhibitor <inhibitor@kaist.ac.kr>
2021-01-16 11:17:05 +0900

    style: attach global style

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 
+import { GlobalStyle } from '../styles';
+
 const App: React.FC = () => {
-  return <div>Hello, world!</div>;
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <div>
+        <h1>Hello, world!</h1>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default hot(App);
